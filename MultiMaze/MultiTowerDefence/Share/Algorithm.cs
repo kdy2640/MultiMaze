079d824777389ec93313cc23c82f303d9e1b02d5
kdy2640 <rlaehduq2640@gmail.com>
2024-04-29 19:55:04 +0900

    [Feat] Add Algorithm class

---
+++ MultiMaze/MultiTowerDefence/Share/Algorithm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeClient.Share
+{
+
+    public class Algorithm
+    {
+        public bool[,] map;
+        GameManager manager;
+        public Algorithm()
+        {
+            manager = GameManager.Instance;
+            map = manager.map.map;
+        }
+        virtual public List<Point> ToArray(Point start, Point end)
+        {
+            return null;
+
+        }
+
+    }
+
+    public class BFS : Algorithm
+    {
+
+        public override List<Point> ToArray(Point start, Point end)
+        {
+            return null;
+            bool[,] temp = new bool[map.GetLength(0), map.GetLength(1)];
+        }
+    }
+    public class DFS : Algorithm
+    {
+
+        public override List<Point> ToArray(Point start, Point end)
+        {
+            return null;
+            bool[,] temp = new bool[map.GetLength(0), map.GetLength(1)];
+        }
+    }
+
+    public class Astar : Algorithm
+    {
+        public override List<Point> ToArray(Point start, Point end)
+        {
+            return null;
+            bool[,] temp = new bool[map.GetLength(0), map.GetLength(1)];
+        }
+    }
+}
