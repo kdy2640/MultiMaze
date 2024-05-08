686f7821886639bfcca273bc3bbd191e1a914aee
PGHOON <qkrrudgns1121@gmail.com>
2024-05-08 23:29:52 +0900

    [Feat] Add drawing Map, Test drawing path

--- MultiMaze/MultiTowerDefence/Scenes/RoundOverScene.cs
+++ MultiMaze/MultiTowerDefence/Scenes/RoundOverScene.cs
@@ -25,18 +25,35 @@ namespace MazeClient
         private void WinnerPath_Draw(object sender, PaintEventArgs e)
         {
             bool[,] map = Manager.map.map;
+            List<Point> path = GameManager.Instance.path;
             Graphics g = e.Graphics;
             int cellSize = 3;
             int mazeHeight = map.GetLength(0);
             int mazeWidth = map.GetLength(1);
 
+            //맵 & 경로 출력 시작 좌표
+            int startX = 100;
+            int startY = 10;
+
+            
             //미로 그리기
-            for(int i = 0; i< mazeHeight; i++)
+            for(int i = 0; i< mazeHeight - 1; i++)
             {
-                for (int j = 0; j < mazeWidth; j++)
+                for (int j = 0; j < mazeWidth - 1; j++)
                 {
                     Color color = map[i,j] ? Color.White : Color.Black;
-                    g.FillRectangle(new SolidBrush(color), j * cellSize, i * cellSize, cellSize, cellSize);
+                    g.FillRectangle(new SolidBrush(color), startX + j * cellSize, startY + i * cellSize, cellSize, cellSize);
+                }
+            }
+
+            //경로 그리기
+            if (path != null && path.Count > 1)
+            {
+                for (int i = 1; i < path.Count; i++)
+                {
+                    Point prev = new Point(startX + path[i - 1].X * cellSize + cellSize / 2, startY + path[i - 1].Y * cellSize + cellSize / 2);
+                    Point curr = new Point(startX + path[i].X * cellSize + cellSize / 2, startY + path[i].Y * cellSize + cellSize / 2);
+                    g.DrawLine(new Pen(Color.Blue, 2), prev, curr);
                 }
             }
         }
