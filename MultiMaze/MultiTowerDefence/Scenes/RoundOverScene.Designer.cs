9c5aff59dc9d89f30fc3880b217e9a9959e9d444
kdy2640 <rlaehduq2640@gmail.com>
2024-05-09 18:15:08 +0900

    [Fix] Change Draw path realTIme

--- MultiMaze/MultiTowerDefence/Scenes/RoundOverScene.Designer.cs
+++ MultiMaze/MultiTowerDefence/Scenes/RoundOverScene.Designer.cs
@@ -41,24 +41,25 @@
             Countdown = new Label();
             timerCountdown = new System.Windows.Forms.Timer(components);
             button1 = new Button();
+            drawTimer = new System.Windows.Forms.Timer(components);
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(177, 54);
-            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Location = new Point(228, 72);
             label1.Name = "label1";
-            label1.Size = new Size(0, 15);
+            label1.Size = new Size(0, 20);
             label1.TabIndex = 6;
             label1.Click += label1_Click;
             // 
             // Winner
             // 
             Winner.AutoSize = true;
-            Winner.Location = new Point(91, 321);
+            Winner.Location = new Point(117, 428);
+            Winner.Margin = new Padding(4, 0, 4, 0);
             Winner.Name = "Winner";
-            Winner.Size = new Size(34, 15);
+            Winner.Size = new Size(42, 20);
             Winner.TabIndex = 8;
             Winner.Text = "승자:";
             Winner.Click += label2_Click;
@@ -66,51 +67,57 @@
             // WinnerName
             // 
             WinnerName.AutoSize = true;
-            WinnerName.Location = new Point(131, 321);
+            WinnerName.Location = new Point(168, 428);
+            WinnerName.Margin = new Padding(4, 0, 4, 0);
             WinnerName.Name = "WinnerName";
-            WinnerName.Size = new Size(0, 15);
+            WinnerName.Size = new Size(0, 20);
             WinnerName.TabIndex = 9;
             // 
             // Time
             // 
             Time.AutoSize = true;
-            Time.Location = new Point(91, 346);
+            Time.Location = new Point(117, 461);
+            Time.Margin = new Padding(4, 0, 4, 0);
             Time.Name = "Time";
-            Time.Size = new Size(58, 15);
+            Time.Size = new Size(72, 20);
             Time.TabIndex = 10;
             Time.Text = "소요시간:";
             // 
             // TimeValue
             // 
             TimeValue.AutoSize = true;
-            TimeValue.Location = new Point(158, 346);
+            TimeValue.Location = new Point(203, 461);
+            TimeValue.Margin = new Padding(4, 0, 4, 0);
             TimeValue.Name = "TimeValue";
-            TimeValue.Size = new Size(0, 15);
+            TimeValue.Size = new Size(0, 20);
             TimeValue.TabIndex = 11;
             // 
             // BackToMain
             // 
-            BackToMain.Location = new Point(534, 377);
+            BackToMain.Location = new Point(687, 503);
+            BackToMain.Margin = new Padding(4);
             BackToMain.Name = "BackToMain";
-            BackToMain.Size = new Size(75, 23);
+            BackToMain.Size = new Size(96, 31);
             BackToMain.TabIndex = 12;
             BackToMain.Text = "나가기";
             BackToMain.UseVisualStyleBackColor = true;
             // 
             // Ready
             // 
-            Ready.Location = new Point(534, 346);
+            Ready.Location = new Point(687, 461);
+            Ready.Margin = new Padding(4);
             Ready.Name = "Ready";
-            Ready.Size = new Size(75, 23);
+            Ready.Size = new Size(96, 31);
             Ready.TabIndex = 13;
             Ready.Text = "준비";
             Ready.UseVisualStyleBackColor = true;
             // 
             // Start
             // 
-            Start.Location = new Point(534, 317);
+            Start.Location = new Point(687, 423);
+            Start.Margin = new Padding(4);
             Start.Name = "Start";
-            Start.Size = new Size(75, 23);
+            Start.Size = new Size(96, 31);
             Start.TabIndex = 14;
             Start.Text = "시작";
             Start.UseVisualStyleBackColor = true;
@@ -118,37 +125,47 @@
             // NextTime
             // 
             NextTime.AutoSize = true;
-            NextTime.Location = new Point(536, 291);
+            NextTime.Location = new Point(689, 388);
+            NextTime.Margin = new Padding(4, 0, 4, 0);
             NextTime.Name = "NextTime";
-            NextTime.Size = new Size(46, 15);
+            NextTime.Size = new Size(57, 20);
             NextTime.TabIndex = 15;
             NextTime.Text = "카운트:";
             // 
             // Countdown
             // 
             Countdown.AutoSize = true;
-            Countdown.Location = new Point(588, 291);
+            Countdown.Location = new Point(756, 388);
+            Countdown.Margin = new Padding(4, 0, 4, 0);
             Countdown.Name = "Countdown";
-            Countdown.Size = new Size(14, 15);
+            Countdown.Size = new Size(17, 20);
             Countdown.TabIndex = 16;
             Countdown.Text = "5";
             // 
+            // timerCountdown
+            // 
+            timerCountdown.Tick += timerCountdown_Tick;
+            // 
             // button1
             // 
-            button1.Location = new Point(515, 11);
-            button1.Margin = new Padding(2);
+            button1.Location = new Point(662, 15);
             button1.Name = "button1";
-            button1.Size = new Size(127, 22);
+            button1.Size = new Size(163, 29);
             button1.TabIndex = 5;
             button1.Text = "6번 화면 이동";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // drawTimer
+            // 
+            drawTimer.Interval = 10;
+            drawTimer.Tick += drawTimer_Tick;
+            // 
             // RoundOverScene
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(710, 427);
+            ClientSize = new Size(913, 569);
             Controls.Add(Countdown);
             Controls.Add(NextTime);
             Controls.Add(Start);
@@ -160,7 +177,6 @@
             Controls.Add(Winner);
             Controls.Add(label1);
             Controls.Add(button1);
-            Margin = new Padding(2);
             Name = "RoundOverScene";
             Text = "RoundOver";
             Load += RoundOverScene_Load;
@@ -182,5 +198,6 @@
         private Label Countdown;
         private System.Windows.Forms.Timer timerCountdown;
         private Button button1;
+        private System.Windows.Forms.Timer drawTimer;
     }
 }
--- MultiMaze/MultiTowerDefence/Scenes/RoundOverScene.cs
+++ MultiMaze/MultiTowerDefence/Scenes/RoundOverScene.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,49 +16,71 @@ namespace MazeClient
     public partial class RoundOverScene : Form
     {
         GameManager Manager;
+
+        bool[,] map;
+        List<Point> path;
+        int cellSize = 4;
+        int mazeHeight;
+        int mazeWidth;
+
+        //맵 & 경로 출력 시작 좌표
+        int startX = 100;
+        int startY = 10;
         public RoundOverScene()
         {
             InitializeComponent();
             Manager = GameManager.Instance;
             this.Paint += new PaintEventHandler(WinnerPath_Draw);
+            this.DoubleBuffered = true; // 로딩 잘 되게 합니다.
+            map = Manager.map.map;
+            path = Manager.path;
+            cellSize = 4;
+            mazeHeight = map.GetLength(0);
+            mazeWidth = map.GetLength(1);
+            drawTimer.Start();
         }
 
+        int count = 0;
         private void WinnerPath_Draw(object sender, PaintEventArgs e)
         {
-            bool[,] map = Manager.map.map;
-            List<Point> path = GameManager.Instance.path;
+
             Graphics g = e.Graphics;
-            int cellSize = 3;
-            int mazeHeight = map.GetLength(0);
-            int mazeWidth = map.GetLength(1);
 
-            //맵 & 경로 출력 시작 좌표
-            int startX = 100;
-            int startY = 10;
 
-            
-            //미로 그리기
-            for(int i = 0; i< mazeHeight - 1; i++)
+            //미로 그리기 
+            for (int i = 0; i < mazeHeight - 1; i++)
             {
                 for (int j = 0; j < mazeWidth - 1; j++)
                 {
-                    Color color = map[i,j] ? Color.White : Color.Black;
-                    g.FillRectangle(new SolidBrush(color), startX + j * cellSize, startY + i * cellSize, cellSize, cellSize);
+                    Color color = map[i, j] ? Color.White : Color.Black;
+                    g.FillRectangle(new SolidBrush(color), startX + i * cellSize, startY + j * cellSize, cellSize, cellSize);
                 }
             }
 
             //경로 그리기
+            Pen pen = new Pen(Color.Red, 2);
             if (path != null && path.Count > 1)
             {
-                for (int i = 1; i < path.Count; i++)
-                {
-                    Point prev = new Point(startX + path[i - 1].X * cellSize + cellSize / 2, startY + path[i - 1].Y * cellSize + cellSize / 2);
-                    Point curr = new Point(startX + path[i].X * cellSize + cellSize / 2, startY + path[i].Y * cellSize + cellSize / 2);
-                    g.DrawLine(new Pen(Color.Blue, 2), prev, curr);
-                }
+                for (int i = 1; i < count; i++)
+                { 
+                    Point prev = CalculatePoint(i - 1);
+                    Point curr = CalculatePoint(i);
+                    g.DrawLine(pen, prev, curr);
+                } 
             }
-        }
 
+
+            count++; 
+            if(count == path.Count)
+            {
+                count = 0;
+            }
+
+        }
+        private Point CalculatePoint(int index)
+        {
+            return new Point(startX + path[index].X * cellSize + cellSize / 2, startY + path[index].Y * cellSize + cellSize / 2);
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             Manager.scene.ChangeGameState(this, Define.GameState.GameOverScene);
@@ -76,6 +99,15 @@ namespace MazeClient
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void timerCountdown_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private void drawTimer_Tick(object sender, EventArgs e)
+        { 
+            this.Invalidate();
         }
     }
 }
