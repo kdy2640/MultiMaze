55f7b15ac721a15a2c22b521255ddcdace3fb858
PGHOON <qkrrudgns1121@gmail.com>
2024-05-26 23:14:48 +0900

    [Feat] Tune Design Gameover

--- MultiMaze/MultiTowerDefence/Scenes/GameOverScene.Designer.cs
+++ MultiMaze/MultiTowerDefence/Scenes/GameOverScene.Designer.cs
@@ -28,134 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
             BackToMain = new Button();
+            pictureBox3 = new PictureBox();
+            roundLabel = new Label();
+            panel1 = new Panel();
             First = new Label();
             Second = new Label();
             Third = new Label();
-            FirstName = new Label();
-            SecondName = new Label();
-            ThirdName = new Label();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(312, 61);
-            label1.Name = "label1";
-            label1.Size = new Size(0, 20);
-            label1.TabIndex = 8;
             // 
             // BackToMain
             // 
-            BackToMain.Location = new Point(688, 404);
-            BackToMain.Margin = new Padding(4);
+            BackToMain.Location = new Point(408, 395);
+            BackToMain.Margin = new Padding(2);
             BackToMain.Name = "BackToMain";
-            BackToMain.Size = new Size(96, 31);
-            BackToMain.TabIndex = 9;
+            BackToMain.Size = new Size(124, 68);
+            BackToMain.TabIndex = 19;
             BackToMain.Text = "나가기";
             BackToMain.UseVisualStyleBackColor = true;
             BackToMain.Click += BackToMain_Click;
             // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.FromArgb(64, 64, 64);
+            pictureBox3.Location = new Point(414, 400);
+            pictureBox3.Margin = new Padding(2);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(124, 68);
+            pictureBox3.TabIndex = 21;
+            pictureBox3.TabStop = false;
+            // 
+            // roundLabel
+            // 
+            roundLabel.AutoSize = true;
+            roundLabel.BackColor = Color.Gray;
+            roundLabel.Font = new Font("맑은 고딕", 24F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            roundLabel.ForeColor = Color.LightGray;
+            roundLabel.Location = new Point(23, 14);
+            roundLabel.Margin = new Padding(2, 0, 2, 0);
+            roundLabel.Name = "roundLabel";
+            roundLabel.Size = new Size(191, 45);
+            roundLabel.TabIndex = 22;
+            roundLabel.Text = "라운드 종료";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Gray;
+            panel1.Controls.Add(Third);
+            panel1.Controls.Add(First);
+            panel1.Controls.Add(Second);
+            panel1.Location = new Point(23, 363);
+            panel1.Margin = new Padding(2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(355, 105);
+            panel1.TabIndex = 23;
+            // 
             // First
             // 
             First.AutoSize = true;
-            First.Location = new Point(118, 20);
-            First.Margin = new Padding(4, 0, 4, 0);
+            First.BackColor = Color.LightGray;
+            First.Location = new Point(12, 16);
             First.Name = "First";
-            First.Size = new Size(35, 20);
-            First.TabIndex = 10;
-            First.Text = "1등:";
+            First.Size = new Size(37, 15);
+            First.TabIndex = 8;
+            First.Text = "1등 : ";
             // 
             // Second
             // 
             Second.AutoSize = true;
-            Second.Location = new Point(118, 61);
-            Second.Margin = new Padding(4, 0, 4, 0);
+            Second.BackColor = Color.LightGray;
+            Second.Location = new Point(12, 46);
             Second.Name = "Second";
-            Second.Size = new Size(35, 20);
-            Second.TabIndex = 11;
-            Second.Text = "2등:";
+            Second.Size = new Size(37, 15);
+            Second.TabIndex = 10;
+            Second.Text = "2등 : ";
             // 
             // Third
             // 
             Third.AutoSize = true;
-            Third.Location = new Point(118, 100);
-            Third.Margin = new Padding(4, 0, 4, 0);
+            Third.BackColor = Color.LightGray;
+            Third.Location = new Point(12, 76);
             Third.Name = "Third";
-            Third.Size = new Size(35, 20);
-            Third.TabIndex = 12;
-            Third.Text = "3등:";
-            // 
-            // FirstName
-            // 
-            FirstName.AutoSize = true;
-            FirstName.Location = new Point(163, 20);
-            FirstName.Margin = new Padding(4, 0, 4, 0);
-            FirstName.Name = "FirstName";
-            FirstName.Size = new Size(0, 20);
-            FirstName.TabIndex = 13;
-            // 
-            // SecondName
-            // 
-            SecondName.AutoSize = true;
-            SecondName.Location = new Point(163, 61);
-            SecondName.Margin = new Padding(4, 0, 4, 0);
-            SecondName.Name = "SecondName";
-            SecondName.Size = new Size(0, 20);
-            SecondName.TabIndex = 14;
-            // 
-            // ThirdName
-            // 
-            ThirdName.AutoSize = true;
-            ThirdName.Location = new Point(163, 100);
-            ThirdName.Margin = new Padding(4, 0, 4, 0);
-            ThirdName.Name = "ThirdName";
-            ThirdName.Size = new Size(0, 20);
-            ThirdName.TabIndex = 15;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(118, 124);
-            pictureBox1.Margin = new Padding(4);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(559, 259);
-            pictureBox1.TabIndex = 16;
-            pictureBox1.TabStop = false;
+            Third.Size = new Size(37, 15);
+            Third.TabIndex = 11;
+            Third.Text = "3등 : ";
             // 
             // GameOverScene
             // 
-            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 451);
-            Controls.Add(pictureBox1);
-            Controls.Add(ThirdName);
-            Controls.Add(SecondName);
-            Controls.Add(FirstName);
-            Controls.Add(Third);
-            Controls.Add(Second);
-            Controls.Add(First);
+            BackColor = Color.DimGray;
+            ClientSize = new Size(590, 472);
+            Controls.Add(panel1);
+            Controls.Add(roundLabel);
             Controls.Add(BackToMain);
-            Controls.Add(label1);
+            Controls.Add(pictureBox3);
+            Margin = new Padding(2);
             Name = "GameOverScene";
             Text = "GameOver";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Label label1;
         private Button BackToMain;
+        private PictureBox pictureBox3;
+        private Label roundLabel;
+        private Panel panel1;
+        private Label Third;
         private Label First;
         private Label Second;
-        private Label Third;
-        private Label FirstName;
-        private Label SecondName;
-        private Label ThirdName;
-        private PictureBox pictureBox1;
     }
 }
